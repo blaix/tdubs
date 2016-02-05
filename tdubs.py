82294f60b4ed042a0563ae912e4c9817ffc70fe8
Justin Blake <justin@hentzia.com>
2016-02-05 05:28:00 -0500

    Flesh out docstrings
    Closes #8

--- tdubs.py
+++ tdubs.py
@@ -1,5 +1,32 @@
 class Stub(object):
+    """A test double that can stand in for another object in your test.
+
+    Use this when the logic you are testing depends on the state or return
+    value of a collaborator.
+
+    See `README.rst` for details.
+
+    """
     def __init__(self, _name=None, **kwargs):
+        """Initialize a stub, optionally with a name and/or attributes.
+
+        >>> Stub()
+        <Stub name='' ...>
+
+        >>> Stub('my stub')
+        <Stub name='my stub' ...>
+
+        >>> stub = Stub(foo='bar')
+        >>> stub.foo
+        'bar'
+
+        >>> stub = Stub('my stub', foo='bar')
+        >>> stub
+        <Stub name='my stub' ...>
+        >>> stub.foo
+        'bar'
+
+        """
         self._name = _name or ''
         self._dict_items = {}
         self._stubbed_calls = []
@@ -7,6 +34,16 @@ class Stub(object):
             setattr(self, key, value)
 
     def __repr__(self):
+        """Return representation of this test double.
+
+        The name and object id are shown to make test failure output more
+        helpful.
+
+        >>> stub = Stub('my stub')
+        >>> repr(stub)
+        "<Stub name='my stub' id='...'>"
+
+        """
         return "<%s name='%s' id='%s'>" % (
             self.classname, self._name, id(self))
 
@@ -15,18 +52,44 @@ class Stub(object):
         return type(self).__name__
 
     def __getattr__(self, name):
+        """For unknown attributes: create, set, and return a new double.
+
+        >>> stub = Stub()
+        >>> stub.foo
+        <Stub name='foo' ...>
+
+        """
         attribute = type(self)(name)
         setattr(self, name, attribute)
         return attribute
 
     def __getitem__(self, key):
+        """Provide dict behavior similar to attribute behavior.
+
+        Assigned keys will return the assigned value. New keys will set and
+        return a new double.
+
+        >>> stub = Stub()
+        >>> stub['foo'] = 'foo'
+        >>> stub['foo']
+        'foo'
+        >>> stub['bar']
+        <Stub name='bar' ...>
+
+        """
         self._dict_items.setdefault(key, type(self)(key))
         return self._dict_items[key]
 
     def __setitem__(self, key, value):
+        """Assign a specific value for this key."""
         self._dict_items[key] = value
 
     def __call__(self, *args, **kwargs):
+        """Return stubbed value for this call.
+
+        Raises TypeError if there is no stubbed value for this call.
+
+        """
         actual_call = Call(*args, **kwargs)
         stubbed_call = next(
             (c for c in self._stubbed_calls if c == actual_call), None)
@@ -56,11 +119,24 @@ calling = Stub._stub_call
 
 
 class Mock(Stub):
+    """A test double that stands in for a delegate callable in your test.
+
+    Provides all the behavior of Stub, but is callable by default, and records
+    calls for verification.
+
+    Use a Mock when you are testing behavior that includes delegating to some
+    other callable.
+
+    See `README.rst` for details.
+
+    """
     def __init__(self, *args, **kwargs):
+        """Initialize a new Mock. See `Stub.__init__` for details."""
         super(Mock, self).__init__(*args, **kwargs)
         self._actual_calls = []
 
     def _handle_call(self, actual_call, stubbed_call):
+        """Extends Stub call handling behavior to be callable by default."""
         self._actual_calls.append(actual_call)
         use_call = stubbed_call or actual_call
         return use_call.return_value
@@ -83,46 +159,71 @@ calls = Mock._get_calls
 class Call(object):
     """A representation of a function call.
 
-    >>> call = Call()
-    >>> call.args
-    ()
-    >>> call.kwargs
-    {}
-
-    >>> call.passing('foo', 'bar', baz='baz')
-    <Call args=('foo', 'bar') kwargs={'baz': 'baz'}>
-    >>> call.args
-    ('foo', 'bar')
-    >>> call.kwargs
-    {'baz': 'baz'}
-
-    >>> call.returns('value')
-    >>> call.return_value
-    'value'
-
-    >>> call = Call('foo', bar='bar')
-    >>> call == Call('asdf', bar='bar')
-    False
-    >>> call == Call('foo', bar='asdf')
-    False
-    >>> call == Call('foo', bar='bar')
-    True
+    A call is identified by its args and kwargs.
 
     """
     def __init__(self, *args, **kwargs):
+        """Initialize a call. Optionally with args and/or kwargs.
+
+        >>> call = Call()
+        >>> call.args
+        ()
+        >>> call.kwargs
+        {}
+
+        >>> call = Call('foo', bar='bar')
+        >>> call.args
+        ('foo',)
+        >>> call.kwargs
+        {'bar': 'bar'}
+
+        """
         self.return_value = Mock()
         self.args = args
         self.kwargs = kwargs
 
     def __repr__(self):
+        """Represent a Call by its args and kwargs.
+
+        >>> call = Call('foo', bar='bar')
+        >>> repr(call)
+        "<Call args=('foo',) kwargs={'bar': 'bar'}>"
+
+        """
         return '<Call args=%s kwargs=%s>' % (self.args, self.kwargs)
 
     def __eq__(self, other):
+        """Calls are equal if they have the same args and kwargs.
+
+        This is what lets call verification work in tests. Call stubs and
+        actual calls are saved as a `Call` with the given args and kwargs,
+        and then verified by checking if they are equal with `==`.
+
+        >>> Call() == Call()
+        True
+
+        >>> Call('foo') == Call()
+        False
+
+        >>> Call('foo') == Call('bar')
+        False
+
+        >>> Call('foo') == Call('foo', bar='bar')
+        False
+
+        >>> Call('foo', bar='bar') == Call('foo', bar='bar')
+        True
+
+        """
         return (self.args == other.args) and (self.kwargs == other.kwargs)
 
     @property
     def formatted_args(self):
         """Format call arguments as a string.
+
+        This is used to make test failure messages more helpful by referring
+        to calls using a string that matches how they were, or should have been
+        called.
 
         >>> call = Call('arg1', 'arg2', kwarg='kwarg')
         >>> call.formatted_args
@@ -136,7 +237,7 @@ class Call(object):
     def passing(self, *args, **kwargs):
         """Assign expected call args/kwargs to this call.
 
-        Returns self for the common case of chaining a call to returns().
+        Returns `self` for the common case of chaining a call to `Call.returns`
 
         >>> Call().passing('foo', bar='baz')
         <Call args=('foo',) kwargs={'bar': 'baz'}>
@@ -161,8 +262,29 @@ class Call(object):
 
 
 class Verification(object):
+    """Verification objects are used to verify mocked calls.
+
+    See `README.rst` for details.
+
+    """
     def __init__(self, mock):
+        """Initialize with a mock.
+
+        >>> mock = Mock('my mock')
+        >>> Verification(mock)
+        <Verification mock=<Mock name='my mock' ...>>
+
+        It is also available as `verify` to make tests read better.
+
+        >>> verify(mock)
+        <Verification mock=<Mock name='my mock' ...>>
+
+        """
         self.mock = mock
+
+    def __repr__(self):
+        """Represent a verification by the mock it's wrapping."""
+        return '<Verification mock=%s>' % self.mock
 
     def called(self):
         """Return True if the mock was called.
@@ -216,4 +338,9 @@ verify = Verification
 
 
 class VerificationError(Exception):
+    """Exception raised when verification of a call fails.
+
+    See `Verification` for details or `README.rst` for examples.
+
+    """
     pass
