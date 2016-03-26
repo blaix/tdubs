57b73b60fd342d110ca41528fae70550102b5430
Justin Blake <justin@hentzia.com>
2016-03-26 05:50:50 -0400

    Bump to version 0.2.0

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,8 +1,9 @@
-DEV
-===
+0.2.0
+=====
 
 * **Feature:** Raise exceptions from stubbed calls with ``.raises(exception)``
 * ``.returns()`` can now be called without arguments to stub a call that returns nothing.
+* Officially support Python 2
 
 0.1.2
 =====
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='tdubs',
-    version='0.1.2',
+    version='0.2.0',
     url='https://github.com/blaix/tdubs',
     author='Justin Blake',
     author_email='justin@blaix.com',
@@ -14,14 +14,14 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development :: Testing',
     ],
 )
